d758b88f7be67509940ebb42fafe83c8b4a27b06
Miller <h_miller6@tss-hr420-dm03.fc.ca>
2017-03-10 15:45:16 -0500

    final changes

--- js/master.js
+++ js/master.js
@@ -10,10 +10,23 @@
 		
 		.done(function(data){
 			console.log(data);
-
+//To flip through cars take content out of if else and delete if else statement
+			if (data !== "null") {
+				//if theres a result and its not null (empty)
+			
 			data = JSON.parse(data);
 			//need to do more validation first, and render data
+			
+
+			//animate the thumbnails
+			$('.thumbInfo img').addClass('nonActive'); //jQuery is awesine for this
+			$('#' + data.model).removeClass('nonActive');
 		renderCarData(data);
+			}else {
+				console.log('no data? that is impossible');
+			}
+
+			
 	})
 		//no semicolon yet
 		.fail(function(ajaxCall, status, error) {
